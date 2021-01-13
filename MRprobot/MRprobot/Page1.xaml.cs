8ef7de90e4ad01fc9b5bf3c80b6303e50093531e
enrick627 <enrick.demunter@student.romerocollege.be>
2021-01-13 21:27:30 +0100

    commit

--- MRprobot/MRprobot/Page1.xaml.cs
+++ MRprobot/MRprobot/Page1.xaml.cs
@@ -1,27 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace MRprobot
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
     {
-        public MainPage()
+        public Page1()
         {
             InitializeComponent();
-            
-        }
-        private async void NavigateButton_OnClicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new Arm());
         }
         private async void NavigateButton_OnWielenClicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new Page1());
+            await Navigation.PushAsync(new MainPage());
         }
     }
 }
