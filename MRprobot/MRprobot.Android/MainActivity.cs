af4a70174bc6297cd01ef7a3c5c0015b4b972ba5
enrick627 <enrick.demunter@student.romerocollege.be>
2021-01-21 19:09:53 +0100

    commit tijdens oef nog altijd problemen met bleutooth.

--- MRprobot/MRprobot.Android/MainActivity.cs
+++ MRprobot/MRprobot.Android/MainActivity.cs
@@ -1,21 +1,24 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Bluetooth;
+using System.Linq;
 
 namespace MRprobot.Droid
 {
     [Activity(Label = "MRprobot", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        BleuthootConnectie Connectie = new BleuthootConnectie();
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
+            
 
             base.OnCreate(savedInstanceState);
 
